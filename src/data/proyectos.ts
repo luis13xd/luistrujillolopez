7c92487143c9ffc782e69ef1729517b8107812ae
Luis <trujillolopezluisxd@gmail.com>
2025-10-10 14:14:46 -0500

    agregue links de demos de los 2 proyectopos de la usco

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -59,7 +59,7 @@ export const projectsData: Proyecto[] = [
       "Web usada para que los aspirantes puedan calcular una estimación de su puntaje ponderado de ingreso a una carrera, cuenta con su panel administrador para ajustar puntajes y carreras de la universidad.",
     tecnologias: ["Angular", "Java", "Spring Boot", "PostgreSQL", "Docker"],
     imagen: ponderados,
-    urlDemo: "https://restauranteinfusion.netlify.app/",
+    urlDemo: "https://huacayo.usco.edu.co/ponderados/home/simulator",
     urlCodigo: [
       "https://github.com/luis13xd/frontend-restaurante",
       "https://github.com/luis13xd/backend-restaurante",
@@ -71,7 +71,7 @@ export const projectsData: Proyecto[] = [
       "Plataforma completa para hacer encuestas que analiza las respuestas para conocer la demanda y afinidad real de los estudiantes hacia los programas existentes, arrojan como resultado graficos, tablas y porcentajes.",
     tecnologias: ["Angular", "Java", "Spring Boot", "PostgreSQL", "Docker"],
     imagen: encuesta,
-    urlDemo: "https://restauranteinfusion.netlify.app/",
+    urlDemo: "https://huacayo.usco.edu.co/ponderados/register",
     urlCodigo: [
       "https://github.com/luis13xd/frontend-restaurante",
       "https://github.com/luis13xd/backend-restaurante",
