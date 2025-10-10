481929be6ef1a9ce2c3a8da806b055f06127d0e7
Luis <trujillolopezluisxd@gmail.com>
2025-10-10 14:13:04 -0500

    agregue proyecto de encuestas usco

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -4,6 +4,7 @@ import seller from "../assets/seller.png";
 import lowitapp from "../assets/lowitapp.png";
 import infusion from "../assets/infusion2.png";
 import ponderados from "../assets/ponderados.png";
+import encuesta from "../assets/encuesta.png";
 
 export const projectsData: Proyecto[] = [
   {
@@ -55,9 +56,21 @@ export const projectsData: Proyecto[] = [
   {
     titulo: "Ponderados USCO",
     descripcion:
-      "Simulador de ponderados universitario usado para que los aspirantes puedan calcular una estimación de su puntaje ponderado de ingreso a una carrera específica de la Universidad Surcolombiana",
+      "Web usada para que los aspirantes puedan calcular una estimación de su puntaje ponderado de ingreso a una carrera, cuenta con su panel administrador para ajustar puntajes y carreras de la universidad.",
     tecnologias: ["Angular", "Java", "Spring Boot", "PostgreSQL", "Docker"],
     imagen: ponderados,
+    urlDemo: "https://restauranteinfusion.netlify.app/",
+    urlCodigo: [
+      "https://github.com/luis13xd/frontend-restaurante",
+      "https://github.com/luis13xd/backend-restaurante",
+    ],
+  },
+  {
+    titulo: "Módulo de encuestas P.O.P",
+    descripcion:
+      "Plataforma completa para hacer encuestas que analiza las respuestas para conocer la demanda y afinidad real de los estudiantes hacia los programas existentes, arrojan como resultado graficos, tablas y porcentajes.",
+    tecnologias: ["Angular", "Java", "Spring Boot", "PostgreSQL", "Docker"],
+    imagen: encuesta,
     urlDemo: "https://restauranteinfusion.netlify.app/",
     urlCodigo: [
       "https://github.com/luis13xd/frontend-restaurante",
