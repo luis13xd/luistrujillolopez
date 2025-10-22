807e81bb4e87e50e4d509587f96b349d7cb38117
Luis <trujillolopezluisxd@gmail.com>
2025-10-21 21:58:11 -0500

    ajustamos nombre del nuevo proyecto

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -76,7 +76,7 @@ export const projectsData: Proyecto[] = [
     ],
   },
     {
-    titulo: "Módulo Encuestas P.O.P USCO",
+    titulo: "Portafolio Web",
     descripcion:
       "Plataforma completa para hacer encuestas que analiza las respuestas para conocer la demanda y afinidad real de los estudiantes hacia los programas existentes, arrojan como resultado graficos, tablas y porcentajes.",
     tecnologias: ["Angular", "Java", "Spring Boot", "PostgreSQL", "Docker"],
