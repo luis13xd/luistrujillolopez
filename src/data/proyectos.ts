1fa7ad495df105aeef7d8861d0f42ecc61912207
Luis <trujillolopezluisxd@gmail.com>
2025-10-21 21:55:59 -0500

    ajustamos url del nuevo proyecto en el boton demo

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -75,4 +75,15 @@ export const projectsData: Proyecto[] = [
       "https://github.com/luis13xd/ponderadosapi/tree/main/usco-ponderado-api",
     ],
   },
+    {
+    titulo: "Módulo Encuestas P.O.P USCO",
+    descripcion:
+      "Plataforma completa para hacer encuestas que analiza las respuestas para conocer la demanda y afinidad real de los estudiantes hacia los programas existentes, arrojan como resultado graficos, tablas y porcentajes.",
+    tecnologias: ["Angular", "Java", "Spring Boot", "PostgreSQL", "Docker"],
+    imagen: encuesta,
+    urlDemo: "https://luistrujillo.netlify.app/",
+    urlCodigo: [
+      "https://github.com/luis13xd/ponderadosapi/tree/main/usco-ponderado-api",
+    ],
+  },
 ];
