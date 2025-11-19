2d88f21d246fc09062b877739359378db55eaa04
Luis <trujillolopezluisxd@gmail.com>
2025-11-19 18:21:52 -0500

    agregamos nueva tecnologia

--- src/data/tegnologias.ts
+++ src/data/tegnologias.ts
@@ -13,11 +13,13 @@ import express from "../assets/express.png";
 import git from "../assets/git.png";
 import mysql from "../assets/mysql.png";
 import sql from "../assets/sql.png";
+import net from "../assets/net.png";
 
 export const technologiesData = [
   { nombre: "React", color: "from-cyan-300 to-blue-100", img: react },
   { nombre: "Angular", color: "from-red-500 to-red-100", img: angular },
   { nombre: "TypeScript", color: "from-blue-400 to-blue-100", img: typescript },
+  { nombre: "Git", color: "from-orange-500 to-red-100", img: git },
   { nombre: "Php", color: "from-indigo-400 to-indigo-50", img: php },
   { nombre: "Html", color: "from-orange-400 to-orange-100", img: html },
   { nombre: "Css", color: "from-blue-400 to-blue-100", img: css },
